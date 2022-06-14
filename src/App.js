d2d0144d4928f478204ae0a8bc0929c702dc2093
Cristian <piri.viejo@gmail.com>
2022-06-14 02:55:38 -0300

    Agregamos el componente ItemListContainer, donde le pusimos el titulo de nuestra aplicacion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "/node_modules/bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer title="Bienvenido a la Tienda de Rammstein"/>
     </div>
   );
 }
