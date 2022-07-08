772c2a4e9bf5f063367afa39950bd208e8c2c14a
Cristian <piri.viejo@gmail.com>
2022-07-08 00:39:05 -0300

    Agrego funcionalidad al cartWidget

--- src/components/CardWidget/CardWidget.js
+++ src/components/CardWidget/CardWidget.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Link } from "react-router-dom";
+
+import {Carrito} from "../../CartContext"
 
 // Icono
 import {BsCartFill} from "react-icons/bs"
@@ -7,9 +10,27 @@ import {BsCartFill} from "react-icons/bs"
 import "./CardWidget.css";
 
 const CardWidget = () => {
+
+  const [carr, setCarr] = useContext(Carrito);
+
+  const [total, setTotal] = useState(0)
+
+  useEffect(()=>{
+    let suma = 0
+
+    carr.forEach( item => {
+      suma = suma + item.quantity
+    });
+
+    setTotal(suma)
+  }, [carr])
+
   return (
     <div>
+      <Link to="/cart" className='cartWidget'>
         <BsCartFill className='cart-icon' />
+        {carr[0] && <span className='spanTotal'>{total}</span> }
+      </Link>
     </div>
   )
 }
--- src/components/ItemCartContainer/ItemCartContainer.js
+++ src/components/ItemCartContainer/ItemCartContainer.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 import ItemCart from "../ItemCart/ItemCart";
 
@@ -7,26 +8,35 @@ import { Carrito } from "../../CartContext";
 const ItemCartContainer = () => {
   const [carr, setCarr, addItem, removeItem, clear] = useContext(Carrito);
 
-    const [total, setTotal] = useState(0)
+  const [total, setTotal] = useState(0);
 
-    useEffect(()=>{
-        let suma = 0
+  useEffect(() => {
+    let suma = 0;
 
-        carr.forEach( item => {
-            suma = suma + (parseInt(item.price) * parseInt(item.quantity) )
-        });
+    carr.forEach((item) => {
+      suma = suma + parseInt(item.price) * parseInt(item.quantity);
+    });
 
-        setTotal(suma)
-    }, [])
+    setTotal(suma);
+  }, [carr]);
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-8">
-          <button onClick={() => clear()}>Borrar todo</button>
-          {carr
-            ? carr.map((item) => <ItemCart item={item} key={item.id} />)
-            : null}
+          {carr[0] ? (
+            <>
+              <button onClick={() => clear()}>Borrar todo</button>
+              {carr.map((item) => <ItemCart item={item} key={item.id} />)}
+            </>
+          ) : (
+            <div>
+              <h1>No has seleccionado ningun producto</h1>
+              <Link to="/" className="btn btn-primary">
+                Volver a Comprar
+              </Link>
+            </div>
+          )}
         </div>
         <div className="col-md-4">
           <h1>Total de venta: {total}</h1>
